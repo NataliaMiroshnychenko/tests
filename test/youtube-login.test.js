3a719287961db9674ec35e20ad1d73fa48c4d11f
Natalia <natalexmir@gmail.com>
2020-03-24 21:25:02 +0200

    ggfhjhlk

---
+++ test/youtube-login.test.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+
+describe('Youtube auth epic', function() {
+    describe('Youtube login', function() {
+        it('should allow to fill login and password and authorize', async function() {
+            this.timeout(0)
+
+            const browser = await puppeteer.launch({headless:false,  slowMo: 200})
+
+            const page = await browser.newPage()
+            await page.goto('https://youtube.com')
+            await page.click('#buttons ytd-button-renderer a')
+            await page.waitForSelector('#identifierId')
+            await page.type('#identifierId', 'diana.semenova@gmail.com')
+            await page.click('#identifierNext')
+            await page.waitForSelector('#password input', { visible: true })
+            await page.type('#password input', 'zimaletto11')
+            await page.click('#passwordNext')
+            return Promise.resolve();
+            //assert.equal([1, 2, 3].indexOf(4), -1);
+        });
+    });
+});
